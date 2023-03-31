1934af627d6ebd384801212850677f874a3827bd
R1C4D0 <2265117944@qq.com>
2023-03-31 12:57:00 +0800

    dont not what to say

--- src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
+++ src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
@@ -1,14 +1,9 @@
 package com.atguigu.admin;
 
-import com.atguigu.admin.view.HelloVIew;
 import com.atguigu.admin.view.MainView;
-import com.atguigu.admin.view.MainView2;
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.atguigu.admin.mapper")
 //@ServletComponentScan(basePackages = "com.atguigu.admin")
--- src/main/java/com/atguigu/admin/view/MainView2.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.admin.view;
-
-import de.felixroske.jfxsupport.AbstractFxmlView;
-import de.felixroske.jfxsupport.FXMLView;
-
-@FXMLView("/fxml/main2.fxml")
-public class MainView2 extends AbstractFxmlView {
-}
